7431e8af47cde2fb4702bd2aa0b3bb570b72bb20
Patryk <patryknartowski@wp.pl>
2025-02-09 17:39:28 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <Sidebar />
+    <div class="content">
+      <MainPanel />
+      <ReservationsPanel />
+      <ClientsPanel />
+      <RoomsPanel />
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+import Sidebar from "./components/Sidebar.vue";
+</script>
+<style scoped>
+#app {
+  display: flex;
+  height: 100vh;
+}
+
+.content {
+  flex: 1;
+  padding: 20px;
+  overflow-y: auto;
+}
+</style>
---
+++ src/components/Sidebar.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="sidebar">
+    <ul>
+      <li><a href="#">Panel Główny</a></li>
+      <li><a href="#">Rezerwacje</a></li>
+      <li><a href="#">Klienci</a></li>
+      <li><a href="#">Pokoje</a></li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+.sidebar {
+  width: 200px;
+  background-color: #2c3e50;
+  color: white;
+  padding: 20px;
+}
+
+.sidebar ul {
+  list-style: none;
+  padding: 0;
+}
+
+.sidebar li {
+  margin: 15px 0;
+}
+
+.sidebar a {
+  color: white;
+  text-decoration: none;
+}
+
+.sidebar a:hover {
+  text-decoration: underline;
+}
+</style>
